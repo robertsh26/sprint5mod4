a5a3f39f55c2cdc376b9253a70d0bc4a2be2ecf4
robertsh26 <60856715+robertsh26@users.noreply.github.com>
2024-02-13 16:07:18 -0700

    sprint5mod4

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,7 @@
 async function moduleProject4() {
-
   // 👇 WORK WORK BELOW THIS LINE 👇
+  document.querySelector('#weatherWidget').style.display = 'none'
+
   const footer = document.querySelector('footer')
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
@@ -14,10 +15,61 @@ async function moduleProject4() {
     ["Partly Cloudy", "⛅️"]
   ]
 
-  // 👉 Tasks 1 - 5 go here
+  document.querySelector('#citySelect').addEventListener('change', async evt => {
+    try {
+      evt.target.setAttribute('disabled', 'disabled')
+      document.querySelector('#weatherWidget').style.display = 'none'
+      document.querySelector('.info').textContent = 'Fetching weather data...'
 
+      let city = evt.target.value
+      let res = await axios.get(`/api/weather?city=${city}`)
+      let data = res.data
+
+      document.querySelector('#weatherWidget').style.display = 'block'
+      document.querySelector('.info').textContent = ''
+      evt.target.removeAttribute('disabled')
+
+      document.querySelector('#apparentTemp div:nth-child(2)')
+        .textContent = `${data.current.apparent_temperature}°`
+      document.querySelector('#todayDescription')
+        .textContent = descriptions.find(d => d[0] === data.current.weather_description)[1]
+      document.querySelector('#todayStats div:nth-child(1)')
+        .textContent = `${data.current.temperature_min}°/${data.current.temperature_max}°`
+      document.querySelector('#todayStats div:nth-child(2)')
+        .textContent = `Precipitation: ${data.current.precipitation_probability * 100}%`
+      document.querySelector('#todayStats div:nth-child(3)')
+        .textContent = `Humidity: ${data.current.humidity}%`
+      document.querySelector('#todayStats div:nth-child(4)')
+        .textContent = `Wind: ${data.current.wind_speed}m/s`
+
+      data.forecast.daily.forEach((day, idx) => {
+        let card = document.querySelectorAll('.next-day')[idx]
+
+        let weekDay = card.children[0]
+        let apparent = card.children[1]
+        let minMax = card.children[2]
+        let precipit = card.children[3]
+
+        weekDay.textContent = getDayOfWeek(day.date)
+        apparent.textContent = descriptions.find(d => d[0] === day.weather_description)[1]
+        minMax.textContent = `${day.temperature_min}°/${day.temperature_max}°`
+        precipit.textContent = `Precipitation: ${day.precipitation_probability * 100}%`
+      })
+
+      document.querySelector('#location').firstElementChild.textContent = data.location.city
+    } catch (err) {
+      console.log(err.message)
+    }
+  })
+
+  function getDayOfWeek(dateString) { 
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const date = new Date(dateString);
+    const dayOfWeekIndex = date.getDay();
+    const dayOfWeek = daysOfWeek[dayOfWeekIndex];
+    return dayOfWeek;
+  }
   // 👆 WORK WORK ABOVE THIS LINE 👆
-
 }
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
